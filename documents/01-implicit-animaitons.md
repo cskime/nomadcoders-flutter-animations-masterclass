7f1293d8b67667342d7bb0714b5f0a8d9367c700
Chamsol Kim <kcsol1005@gmail.com>
2024-07-15 19:55:41 +0900

    #1.1 Implicitly animated widgets, AnimatedOpacity, AnimatedAlign

---
+++ documents/01-implicit-animaitons.md
@@ -0,0 +1,13 @@
+# Implicit Animations
+
+- Animation : a **transition** from one state to the other
+- Opacity의 경우 0, 0.1, 0.2, ..., 1.0 으로 서서히 바뀌는 것
+- Implicit : not directly expressed (암묵적인)
+
+## Implicit animation widget
+
+- Animation을 직접 만들지 않아도 widget을 사용하기만 하면 알아서 animation이 만들어지고 실행됨
+- Animation이 실행되지만 animation 관련 코드를 작성하지 않음
+- `Animated~`로 시작하는 widget들을 사용한다.
+  - `AnimatedOpacity`
+  - `AnimatedAlign`
--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,11 +9,45 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Implicit Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              duration: const Duration(seconds: 1),
+              child: AnimatedOpacity(
+                opacity: _visible ? 1 : 0.2,
+                duration: const Duration(seconds: 1),
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text("Go"),
+            ),
+          ],
+        ),
       ),
     );
   }
