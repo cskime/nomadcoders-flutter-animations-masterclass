2453c072703e2572d695c4b836344b985bfd9403
Chamsol Kim <kcsol1005@gmail.com>
2024-07-22 15:09:25 +0900

    #4.1 AnimationController, animateTo(target,curve), lowerBound ~ upperBound 제한 변경 및 초기값 설정

--- documents/04-swiping-gesture.md
+++ documents/04-swiping-gesture.md
@@ -13,3 +13,12 @@
 
 - `Transform` : layout 직전에 지정한 transform을 적용해 주는 widget
 - `Transform.translate` : Translate transform을 적용하는 named constructor
+
+## AnimationController
+
+- `animateTo(target, curve)`
+  - `AnimationController`의 value를 `target` 값으로 animation을 주면서 변경하는 방법
+  - `curve`로 animation curve도 설정 가능
+  - `AnimationController.value`에 직접 값을 넣는건 animation 없이 변경하는 것
+- `AnimationController.value`에 직접 값을 더해 넣을 때, `lowerBound`와 `upperBound`에 제한되므로, 이 값을 더해지는 값의 범위로 바꿔주어야 함
+  - `lowerBound`와 `upperBound`를 바꿀 때, 생성자에서 `value`를 0로 설정해 줘야 초기 위치를 initial position으로 잡을 수 있다.
--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -7,49 +7,61 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
-  double _positionX = 0;
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.sizeOf(context);
+  late final _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 2000),
+    value: 0,
+    lowerBound: size.width * -1,
+    upperBound: size.width,
+  );
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      _positionX += details.delta.dx;
-    });
+    _animationController.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    setState(() {
-      _positionX = 0;
-    });
+    _animationController.animateTo(0, curve: Curves.bounceOut);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Swiping Cards"),
       ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: GestureDetector(
-              onHorizontalDragUpdate: _onHorizontalDragUpdate,
-              onHorizontalDragEnd: _onHorizontalDragEnd,
-              child: Transform.translate(
-                offset: Offset(_positionX, 0),
-                child: Material(
-                  elevation: 10,
-                  color: Colors.red.shade100,
-                  child: SizedBox(
-                    width: size.width * 0.8,
-                    height: size.height * 0.5,
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) => Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: GestureDetector(
+                onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                onHorizontalDragEnd: _onHorizontalDragEnd,
+                child: Transform.translate(
+                  offset: Offset(_animationController.value, 0),
+                  child: Material(
+                    elevation: 10,
+                    color: Colors.red.shade100,
+                    child: SizedBox(
+                      width: size.width * 0.8,
+                      height: size.height * 0.5,
+                    ),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
