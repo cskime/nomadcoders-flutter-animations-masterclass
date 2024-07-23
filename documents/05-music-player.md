5d1155c6d1e1b4b41faa3b1153a286b3581cf269
Chamsol Kim <kcsol1005@gmail.com>
2024-07-23 15:07:26 +0900

    #5.1 AnimationSwitcher, BackdropFilter, ImageFilter.blur - 배경 blur 처리

--- documents/05-music-player.md
+++ documents/05-music-player.md
@@ -7,3 +7,16 @@
 - `PageController.viewportFraction`
   - Page item의 크기 조정
   - 이 page가 view를 얼마나 차지하는지 비율
+
+## AnimatedSwitcher
+
+- Child가 변경되면 animation을 받는 widget
+- `child`에 다른 widget이 들어오면, 이전 widget이 새 widget으로 변환되는 animation을 만들어 준다.
+- 즉, `child`를 이전과 다른 widget으로 변경해 주어야 함
+- Flutter는 widget을 key를 사용해서 식별하므로, `child` widget의 key를 다르게 사용하면 이전/다음 widget을 다른 widget으로 식별할 수 있음
+
+## BackdropFilter
+
+- 아래에 깔린 widget들을 배경으로(backdrop) 만드는 filter를 적용할 수 있는 widget
+- `BackdropFilter.filter`에 설정한 `ImageFilter`는 `child` widget에 적용된다.
+- `ImageFilter.blur(sigmaX,sigmaY)`를 사용하면 gaussian blur filter를 사용할 수 있다.
--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -10,13 +12,48 @@ class MusicPlayerScreen extends StatefulWidget {
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   final _pageController = PageController(viewportFraction: 0.8);
 
+  int _currentPage = 0;
+
+  void _onPageChanged(int value) {
+    setState(() {
+      _currentPage = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // switcher 동작 시 흰색 없애기
       body: Stack(
         children: [
+          Positioned.fill(
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: Container(
+                key: ValueKey(_currentPage),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: AssetImage(
+                        "assets/covers/album-${_currentPage + 1}.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 5,
+              sigmaY: 5,
+            ),
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
           PageView.builder(
             controller: _pageController,
+            onPageChanged: _onPageChanged,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) => Column(
@@ -27,18 +64,34 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage("assets/covers/album-${index + 1}.png"),
+                      fit: BoxFit.cover,
                     ),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: const Offset(0, 8),
+                      )
+                    ],
                   ),
                 ),
                 const SizedBox(height: 32),
                 const Text(
                   "Taylor Swift",
-                  style: TextStyle(fontSize: 24),
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 5),
                 const Text(
                   "Taylor Swift",
-                  style: TextStyle(fontSize: 18),
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
